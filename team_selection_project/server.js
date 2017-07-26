8204975aace3488e0f945b427a0dde51f5a7751b
Srinivas Tenali <stenali@lingotek.com>
2017-07-26 09:34:16 +0530

    team selection

--- team_selection_project/server.js
+++ team_selection_project/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
-   res.sendFile('dist/index.html', {root: __dirname })
+   res.sendFile('public/index.html', {root: __dirname })
 })
 console.log(`server is listening on ${port}`)
 app.listen(port)
